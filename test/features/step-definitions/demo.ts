af3606b1bb103c40dac5928ffd19a68483901f32
juju1123 <joshuaemmanuel.joson@gmail.com>
2023-01-25 14:28:38 +0800

    Added input handling

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -34,3 +34,47 @@ Then(/^URL should match (.*)$/, async function(ExpectedURL){
     let url = await browser.getUrl()
     chai.expect(url).to.equal(ExpectedURL);
 })
+/*
+    Web Interactions
+*/
+
+Given(/^A web page is opened$/, async function() {
+
+    //base URL as per wdio config. any argument inside browser.url() appends it to the base url.
+    await browser.url("/inputs")
+    await browser.setTimeout({
+        implicit: 15000,
+        pageLoad: 10000
+    })
+    await browser.maximizeWindow()
+})
+
+When(/^Perform web interactions$/, async function() {
+/*
+    1. Input Box
+        Actions
+            1. Type into the input box
+            2. Clear the field and type or just add a value
+            3. Click and type
+            4. Type slowly
+
+*/
+
+    let num = "12345"
+    let strNum = num.toString()
+
+    let ele = await $(`[type=number]`)
+    // await ele.click()
+    // await ele.setValue(strNum)
+    
+    //typing slowly. You need to click it before the automation will type it.
+    await ele.click()
+    for (let i=0; i < strNum.length; i++){
+        let charStr = strNum.charAt(i)
+        await browser.pause(1000)
+        await browser.keys(charStr)
+    }
+
+    await browser.debug()
+
+})
