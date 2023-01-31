ec6d1034b964474d4006c605718537054fb2f188
juju1123 <joshuaemmanuel.joson@gmail.com>
2023-01-31 18:16:27 +0800

    commit again and again

--- test/features/step-definitions/given.ts
+++ test/features/step-definitions/given.ts
@@ -6,7 +6,7 @@ import chai from "chai";
 Given(/^Login to inventory system$/, async function() {
 
     console.log(`Test username: ${process.env.TEST_USERNAME}`)
-    console.log(`Test password: ${process.env.TEST_PASSWORD}`)
+    console.log(`Test password: ${process.env.TEST_PASSWORDD}`)
 
     // Launch browser to inventory app
     await browser.url("https://www.saucedemo.com/")
